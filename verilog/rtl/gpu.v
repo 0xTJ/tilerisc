f474d3de453abe4805b1e26e0130ff1193488201
Thomas Jager <thomasj@thomasjager.ca>
2023-11-28 08:33:06 -0500

    Changes

--- verilog/rtl/gpu.v
+++ verilog/rtl/gpu.v
@@ -13,160 +13,164 @@
 // limitations under the License.
 // SPDX-License-Identifier: Apache-2.0
 
-module gpu (
-    input wire wb_clk_i
+// Each matrix has vectors addressable as:
+// Row x4
+// Col x4
+// Diag x4
+// Anti-diag x4
+
+// Stores use a bit (where?) to decide whether to clear the rest of the matrix
+
+// All values are 4.12 two's-complement fixed-point numbers
+
+`define VECTOR_TYPE_COL         2'b00
+`define VECTOR_TYPE_ROW         2'b01
+`define VECTOR_TYPE_DIAG        2'b10
+`define VECTOR_TYPE_ANTIDIAG    2'b11
+
+module gpu_core #(
+    parameter MAT_COUNT = 4
+) (
+    input  wire clk,
+
+    // Data port
+    input  wire [$clog2(MAT_COUNT)-1:0] dat_mat_idx,
+    input  wire [1:0]                   dat_vector_type,
+    input  wire [1:0]                   dat_vector_idx,
+    input  wire [63:0]                  dat_in,
+    output reg  [63:0]                  dat_out,
+    input  wire                         cyc,
+    input  wire                         we,
+    output wire                         ack,
+
+    // Command port
+    // input  wire [15:0]  command
+    
+    // output reg          busy
+);
+
+reg [1:0] mat_row [3:0];
+reg [1:0] mat_col [3:0];
+
+always @(*) begin
+    case (vector_type)
+        `VECTOR_TYPE_COL: begin
+            mat_row[0] = 2'd0;
+            mat_row[1] = 2'd1;
+            mat_row[2] = 2'd2;
+            mat_row[3] = 2'd3;
+            mat_col[0] = vector_idx;
+            mat_col[1] = vector_idx;
+            mat_col[2] = vector_idx;
+            mat_col[3] = vector_idx;
+        end
+
+        `VECTOR_TYPE_ROW: begin
+            mat_row[0] = vector_idx;
+            mat_row[1] = vector_idx;
+            mat_row[2] = vector_idx;
+            mat_row[3] = vector_idx;
+            mat_col[0] = 2'd0;
+            mat_col[1] = 2'd1;
+            mat_col[2] = 2'd2;
+            mat_col[3] = 2'd3;
+        end
+
+        `VECTOR_TYPE_DIAG: begin
+            mat_row[0] = 2'd0;
+            mat_row[1] = 2'd1;
+            mat_row[2] = 2'd2;
+            mat_row[3] = 2'd3;
+            mat_col[0] = (2'd0 + vector_idx) % 4;
+            mat_col[1] = (2'd1 + vector_idx) % 4;
+            mat_col[2] = (2'd2 + vector_idx) % 4;
+            mat_col[3] = (2'd3 + vector_idx) % 4;
+        end
+
+        `VECTOR_TYPE_ANTIDIAG: begin
+            mat_row[0] = (2'd3 - vector_idx) % 4;
+            mat_row[1] = (2'd2 - vector_idx) % 4;
+            mat_row[2] = (2'd1 - vector_idx) % 4;
+            mat_row[3] = (2'd0 - vector_idx) % 4;
+            mat_col[0] = 2'd0;
+            mat_col[1] = 2'd1;
+            mat_col[2] = 2'd2;
+            mat_col[3] = 2'd3;
+        end
+    endcase
+end
+
+mat_reg_file_mul mat_reg_file_mul (
+    .clk (clk),
+
+    .val_rd_mat_idx (),
+    .val_rd_mat_rows ({mat_row[3], mat_row[2], mat_row[1], mat_row[0]}),
+    .val_rd_mat_cols ({mat_col[3], mat_col[2], mat_col[1], mat_col[0]}),
 );
 
 endmodule
 
-module gpu_core (
+module mat_reg_file_mul #(
+    parameter MAT_COUNT = 4
+) (
+    // Value control
+    input  wire clk,
+    input  wire we,
+
+    input  wire [$clog2(MAT_COUNT)-1:0] val_rd_mat_idx,
+    input  wire [7:0] val_rd_mat_rows,
+    input  wire [7:0] val_rd_mat_cols,
+    output wire [63:0] val_rd_vals,
+    
+    input  wire [$clog2(MAT_COUNT)-1:0] val_wr_mat_idx,
+    input  wire [7:0] val_wr_mat_rows,
+    input  wire [7:0] val_wr_mat_cols,
+    input  reg  [63:0] val_wr_vals,
+
+    // Multiplier control
+    input  wire [$clog2(MAT_COUNT)-1:0]  mul_mat_idx,
+    input  wire [63:0] mul_in,
+    output reg  [63:0] mul_out,
 );
 
-endmodule
-
-module interp_tri (
-`ifdef USE_POWER_PINS
-    inout vdd,	// User area 5.0 V supply
-    inout vss,	// User area digital ground
-`endif
-
-    input  wire wb_clk_i,
-    input  wire wb_rst_i,
-    input  wire wbs_stb_i,
-    input  wire wbs_cyc_i,
-    input  wire wbs_we_i,
-    input  wire [3:0] wbs_sel_i,
-    input  wire [31:0] wbs_dat_i,
-    input  wire [3:2] wbs_adr_i,
-    output reg  wbs_ack_o,
-    output reg  [31:0] wbs_dat_o,
-
-    input wire clk,
-    input wire [7:0] y,
-    output wire [7:0] x_start,
-    output wire [7:0] x_end,
-    output wire active
-);
-
-reg         [7:0]   a [1:0];
-reg         [7:0]   b [1:0];
-reg         [7:0]   c [1:0];
-reg signed  [9:0]   delta_t;
-reg                 bflip;
-
-assign active = active_reg;
+//   bit             mat   row   col
+reg [15:0] mat_regs [MAT_COUNT-1:0] [3:0] [3:0];
 
 always @(*) begin
-    wbs_ack_o = wbs_cyc_i && wbs_stb_i;
-    if (wbs_cyc_i && wbs_stb_i) begin
-        case (wbs_adr_i[3:2])
-            2'b00: wbs_dat_o = {8'h00, a[1], 8'h00, a[0]};
-            2'b01: wbs_dat_o = {8'h00, b[1], 8'h00, b[0]};
-            2'b10: wbs_dat_o = {8'h00, c[1], 8'h00, c[0]};
-            2'b11: wbs_dat_o = {15'h0000, bflip, 6'h0, delta_t};
-        endcase
-    end else begin
-        wbs_dat_o = 32'hxxxxxxxx;
-    end
-end
+    val_rd_vals[ 0+:16] = mat_regs[val_rd_mat_idx][val_rd_mat_rows[0+:2]][val_rd_mat_cols[0+:2]];
+    val_rd_vals[16+:16] = mat_regs[val_rd_mat_idx][val_rd_mat_rows[2+:2]][val_rd_mat_cols[2+:2]];
+    val_rd_vals[32+:16] = mat_regs[val_rd_mat_idx][val_rd_mat_rows[4+:2]][val_rd_mat_cols[4+:2]];
+    val_rd_vals[48+:16] = mat_regs[val_rd_mat_idx][val_rd_mat_rows[6+:2]][val_rd_mat_cols[6+:2]];
 
-always @(posedge wb_clk_i) begin
-    if (wb_rst_i) begin
-        a[0]    <= 8'h00;
-        a[1]    <= 8'h00;
-        b[0]    <= 8'h00;
-        b[1]    <= 8'h00;
-        c[0]    <= 8'h00;
-        c[1]    <= 8'h00;
-        delta_t <= 10'h00;
-        bflip   <= 1'b0;
-    end else if (wbs_cyc_i && wbs_stb_i && wbs_we_i) begin
-        case (wbs_adr_i[3:2])
-            2'b00: begin
-                if (wbs_sel_i[0]) a[0][7:0]     <= wbs_dat_i[7:0];
-                if (wbs_sel_i[2]) a[1][7:0]     <= wbs_dat_i[23:16];
-            end
-            2'b01: begin
-                if (wbs_sel_i[0]) b[0][7:0]     <= wbs_dat_i[7:0];
-                if (wbs_sel_i[2]) b[1][7:0]     <= wbs_dat_i[23:16];
-            end
-            2'b10: begin
-                if (wbs_sel_i[0]) c[0][7:0]     <= wbs_dat_i[7:0];
-                if (wbs_sel_i[2]) c[1][7:0]     <= wbs_dat_i[23:16];
-            end
-            2'b11: begin
-                if (wbs_sel_i[0]) delta_t[7:0]  <= wbs_dat_i[7:0];
-                if (wbs_sel_i[1]) delta_t[9:8]  <= wbs_dat_i[9:8];
-                if (wbs_sel_i[2]) bflip         <= wbs_dat_i[16];
-            end
-        endcase
-    end
-end
-
-// All t values are signed 2.8 fixed-point value
-
-// Determine if this is the start and/or end of this triangle
-reg is_first_line, is_last_line;
-always @(*) begin
-    if (bflip == 1'b0) begin
-        is_first_line   = (y == a[1]);
-        is_last_line    = (y == b[1]);
-    end else begin
-        is_first_line   = (y == b[1]);
-        is_last_line    = (y == a[1]);
-    end
-end
-
-reg active_reg;
-reg signed [9:0] t_reg;
-
-// On the first line, reset to start the triangle
-reg active_in_actual;
-reg [9:0] t_in_actual;
-always @(*) begin
-    if (is_first_line) begin
-        active_in_actual = 1'b1;
-        t_in_actual = {1'h0, bflip, 8'h00};
-    end else begin
-        active_in_actual = active_reg;
-        t_in_actual = t_reg;
-    end
-end
-
-// Half-step in t
-wire signed [9:0] t;
-assign t = t_in_actual + (delta_t >>> 1);
-
-// TODO: This truncates everywhere
-
-// Signed 10.4 fixed-point values
-wire signed [13:0] t_ext;
-reg  signed [13:0] t_mul_a_x, t_mul_b_x, t_mul_c_x;
-reg signed [13:0] x_start_adj_fxp, x_end_adj_fxp;
-assign t_ext = {{8{t[9]}}, t[9:4]};
-
-reg signed [7:0] x_start_adj, x_end_adj;
-
-always @(*) begin
-    t_mul_a_x = t_ext * {6'b0, a[0]};
-    t_mul_b_x = t_ext * {6'b0, b[0]};
-    t_mul_c_x = t_ext * {6'b0, c[0]};
-
-    x_start_adj_fxp = t_mul_c_x - t_mul_a_x;
-    x_end_adj_fxp   = t_mul_b_x - t_mul_a_x;
-
-    // TODO: Add checks
-    x_start_adj = x_start_adj_fxp[11:4];
-    x_end_adj   = x_end_adj_fxp[11:4];
+    mul_out[ 0+:16] =
+        mat_regs[mul_mat_idx][0][0] * mul_in[ 0+:16] +
+        mat_regs[mul_mat_idx][0][1] * mul_in[16+:16] +
+        mat_regs[mul_mat_idx][0][2] * mul_in[32+:16] +
+        mat_regs[mul_mat_idx][0][3] * mul_in[48+:16];
+    mul_out[16+:16] =
+        mat_regs[mul_mat_idx][1][0] * mul_in[ 0+:16] +
+        mat_regs[mul_mat_idx][1][1] * mul_in[16+:16] +
+        mat_regs[mul_mat_idx][1][2] * mul_in[32+:16] +
+        mat_regs[mul_mat_idx][1][3] * mul_in[48+:16];
+    mul_out[32+:16] =
+        mat_regs[mul_mat_idx][2][0] * mul_in[ 0+:16] +
+        mat_regs[mul_mat_idx][2][1] * mul_in[16+:16] +
+        mat_regs[mul_mat_idx][2][2] * mul_in[32+:16] +
+        mat_regs[mul_mat_idx][2][3] * mul_in[48+:16];
+    mul_out[48+:16] =
+        mat_regs[mul_mat_idx][3][0] * mul_in[ 0+:16] +
+        mat_regs[mul_mat_idx][3][1] * mul_in[16+:16] +
+        mat_regs[mul_mat_idx][3][2] * mul_in[32+:16] +
+        mat_regs[mul_mat_idx][3][3] * mul_in[48+:16];
 end
 
 always @(posedge clk) begin
-    x_start <= a[0] + x_start_adj;
-    x_end   <= a[0] + x_end_adj;
-
-    // Update out values to be used on the next line
-    active_reg  <= active_in_actual && !is_last_line;
-    t_reg       <= t_in_actual + delta_t;
+    if (we) begin
+        mat_regs[val_wr_mat_idx][val_wr_mat_rows[0+:2]][val_wr_mat_cols[0+:2]] = val_wr_vals[ 0+:16];
+        mat_regs[val_wr_mat_idx][val_wr_mat_rows[2+:2]][val_wr_mat_cols[2+:2]] = val_wr_vals[16+:16];
+        mat_regs[val_wr_mat_idx][val_wr_mat_rows[4+:2]][val_wr_mat_cols[4+:2]] = val_wr_vals[32+:16];
+        mat_regs[val_wr_mat_idx][val_wr_mat_rows[6+:2]][val_wr_mat_cols[6+:2]] = val_wr_vals[48+:16];
+    end
 end
 
 endmodule
